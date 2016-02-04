24595c623a5efd04ae47962d8f3df03bf5aefcbf
Vitaly Harisov <vitaly@yandex-team.ru>
2016-02-04 22:58:00 +0300

    tests for default scheme and default naming

--- test/test.js
+++ test/test.js
@@ -14,16 +14,57 @@ describe('bem-tools-create', function() {
         mock.restore();
     });
 
-    it('should create a block', function(done) {
-        mock({
-            node_modules: nodeModules
+    describe('default scheme and default naming', function() {
+        it('should create a block using \'nested\' scheme and default naming', function(done) {
+            mock({
+                node_modules: nodeModules
+            });
+
+            create([{ block: 'b' }], ['.'], ['css'])
+                .then(function() {
+                    fs.exists('b/b.css', function(exists) {
+                        exists ? done() : done(new Error(' '));
+                    });
+                }, done);
         });
 
-        create([{ block: 'b1' }], ['.'], ['css'])
-            .then(function() {
-                fs.exists('b1/b1.css', function(exists) {
-                    exists ? done() : done(new Error(' '));
-                });
-            }, done);
+        it('should create an element using \'nested\' scheme and default naming', function(done) {
+            mock({
+                node_modules: nodeModules
+            });
+
+            create([{ block: 'b', elem: 'e' }], ['.'], ['css'])
+                .then(function() {
+                    fs.exists('b/__e/b__e.css', function(exists) {
+                        exists ? done() : done(new Error(' '));
+                    });
+                }, done);
+        });
+
+        it('should create an block modifier using \'nested\' scheme and default naming', function(done) {
+            mock({
+                node_modules: nodeModules
+            });
+
+            create([{ block: 'b', modName: 'm', modVal: 'v' }], ['.'], ['css'])
+                .then(function() {
+                    fs.exists('b/_m/b_m_v.css', function(exists) {
+                        exists ? done() : done(new Error(' '));
+                    });
+                }, done);
+        });
+
+        it('should create an element modifier using \'nested\' scheme and default naming', function(done) {
+            mock({
+                node_modules: nodeModules
+            });
+
+            create([{ block: 'b', elem: 'e', modName: 'em', modVal: 'ev'}], ['.'], ['css'])
+                .then(function() {
+                    fs.exists('b/__e/_em/b__e_em_ev.css', function(exists) {
+                        exists ? done() : done(new Error(''));
+                    });
+                }, done);
+        });
     });
 });
